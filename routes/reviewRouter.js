387f90a8c2139bc08b135e9771db79d2cbc5392a
iamprabo01 <prabodiniwarthana45@gmail.com>
2025-01-05 00:43:01 +0530

    reviewRouter updated

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,10 +1,23 @@
 import express from "express"
-import {addReview, getReviews} from "../controller/reviewController.js";
+import {addReview, deleteReview, getReviews} from "../controller/reviewController.js";
 
 const reviewRouter = express.Router();
 
 reviewRouter.post("/",addReview)
 reviewRouter.get("/",getReviews)
+reviewRouter.delete("/:email",deleteReview)
+
+/*reviewRouter.get("/approved",
+    (req,res)=>{
+        console.log("this is approved route")
+    }
+)
+
+reviewRouter.get("/:email",
+    (req,res)=>{
+        console.log("this is email route")
+    }
+)*/
 
 
 export default reviewRouter;
